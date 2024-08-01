30454c05d61c17f8aa68349cdf009ec00754af32
AkiMoo <32764968+AkiMoo@users.noreply.github.com>
2024-08-02 03:20:37 +0800

    Update 1. 两数之和.cpp

--- 1. 两数之和.cpp
+++ 1. 两数之和.cpp
@@ -1,32 +1,21 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <windows.h>
-using namespace std;
-
-void twoSum(vector<int>& nums, int target) {
-    vector<int> ans;
-    int n = nums.size();
-    unordered_map<int,int> hash;
-    for(int i = 0;i<n;i++){
-        hash[nums[i]] = i+1;
-        if(hash[target-nums[i]]>0){
-            ans.push_back(hash[target-nums[i]]-1),ans.push_back(i);
-            //return ans;
-            break;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        int n = nums.size();
+        unordered_map<int,int> hash;
+        for(int i = 0;i<n;i++){
+            if(hash[target-nums[i]]>0){
+                if(hash[target-nums[i]]-1!=i){
+                    ans.push_back(hash[target-nums[i]]-1),ans.push_back(i);
+                    return ans;
+                }
+            }
+            hash[nums[i]] = i+1;
         }
+
+        ans.push_back(0);
+        ans.push_back(1);
+        return ans;
     }
-    for(int i = 0;i<2;i++){
-        cout<<"fadsfas"<<endl;
-        cout<<ans[i]<<endl;
-    }
-    // ans.push_back(0);
-    // ans.push_back(1);
-    //return ans;
-}
-int main(){
-    vector<int> nums = {2,8,11,4,15,5};
-    
-    twoSum(nums,9);
-    system("pause");
-}
+};
