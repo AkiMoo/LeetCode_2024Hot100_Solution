199e04cc17263a89347d48d0b114cb119b9b078e
AkiMoo <32764968+AkiMoo@users.noreply.github.com>
2024-08-05 01:44:53 +0800

    Update 128. 最长连续序列.cpp

--- 128. 最长连续序列.cpp
+++ 128. 最长连续序列.cpp
@@ -1,21 +1,48 @@
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+//         unordered_map<int,int> hash;
+//         int n = nums.size();
+//         for(int i=0;i<n;i++){
+//             hash[nums[i]] = 1;
+//         }
+//         int ans = 1;
+//         for(int i=0;i<n;i++){
+//             int temp = nums[i];
+//             int thisround = 1;
+//             while(hash[temp++]>0){
+//                 thisround++;
+//                 //temp++;
+//             }
+//             ans = max(ans,thisround);
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_map<int,int> hash;
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            hash[nums[i]] = 1;
+        unordered_set<int> num_set;
+        for (const int& num : nums) {
+            num_set.insert(num);
         }
-        int ans = 1;
-        for(int i=0;i<n;i++){
-            int temp = nums[i];
-            int thisround = 1;
-            while(hash[temp++]>0){
-                thisround = ++;
-                //temp++;
+
+        int longestStreak = 0;
+
+        for (const int& num : num_set) {
+            if (!num_set.count(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (num_set.count(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = max(longestStreak, currentStreak);
             }
-            ans = max(ans,thisround);
         }
-        return ans;
+
+        return longestStreak;           
     }
 };
