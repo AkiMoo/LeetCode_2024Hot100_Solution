d4714f08937f05212372b18d835b17551d91afa4
AkiMoo <32764968+AkiMoo@users.noreply.github.com>
2024-08-08 01:52:11 +0800

    Update 跳跃游戏I&II.cpp

--- 跳跃游戏I&II.cpp
+++ 跳跃游戏I&II.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
-    int canjump(vector<int>& nums) {
+    int jump(vector<int>& nums) {
         int n = nums.size();
         vector<int> hash(n,0);
-        hash[0] = 1;
+        // if(n==1){
+        //     return 0;
+        // }
         for(int i=0;i<n;i++){
-            for(int j =i+1;j<i+nums[i];j++){
-                if(nums[j]==0){
-                    hash[j]=1;
+            if(hash[i]==0&&i>0){
+                break;
+            }
+            for(int j =i+1;j<=i+nums[i]&&j<n;j++){
+                if(hash[j]==0){
+                    hash[j] = hash[i]+1;
                 }
-                else {
+                else{
                     hash[j] = min(hash[j],hash[i]+1);
                 }
             }
